f0161d3ef58da4e5fc3113b36359b99dda6038ce
JR <ranjan.net14@gmail.com>
2021-07-15 09:22:08 +0530

    Routing Session 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {RatingModule} from 'primeng/rating';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { MaterialModule } from './UILib/material.module';
 
 import { AppComponent } from './app.component';
 import { StringInterpolationComponent } from './string-interpolation/string-interpolation.component';
@@ -11,16 +13,21 @@ import { TwowayBindingComponent } from './twoway-binding/twoway-binding.componen
 import { FormsModule } from '@angular/forms';
 import { ProductsComponent } from './products/products.component';
 import { ProductThumbnailComponent } from './product-thumbnail/product-thumbnail.component';
-import { HeaderComponent } from './header/header.component';
 import { ShortenPipe } from './shared/pipe/shorten.pipe';
 import { FilterPipe } from './shared/pipe/filter.pipe';
 import { BasicHighlightDirective } from './shared/directives/basic-highlight.directive';
 import { BetterHighlightDirective } from './shared/directives/better-highlight.directive';
 import { Logger } from './shared/services/logger.service';
-import { ProductService } from './shared/services/product.service';
 import { TempProductsComponent } from './temp-products/temp-products.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
-
+const routes:Routes = [
+    {path:'home',component:HomeComponent},
+    {path:'products', component:ProductsComponent},
+    {path:'',component:HomeComponent},
+    {path:'**',redirectTo:'home'},
+];
 
 @NgModule({
     declarations:[
@@ -31,18 +38,21 @@ import { TempProductsComponent } from './temp-products/temp-products.component';
         TwowayBindingComponent,
         ProductsComponent,
         ProductThumbnailComponent,
-        HeaderComponent,
         ShortenPipe,
         FilterPipe,
         BasicHighlightDirective,
         BetterHighlightDirective,
-        TempProductsComponent
+        TempProductsComponent,
+        HeaderComponent,
+        HomeComponent
     ], 
     imports:[
         BrowserModule,
         BrowserAnimationsModule,
         FormsModule,
-        RatingModule
+        RatingModule,
+        RouterModule.forRoot(routes),
+        MaterialModule
     ],
     providers:[
         Logger
